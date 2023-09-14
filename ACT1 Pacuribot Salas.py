43e05e63fd3702839d11707ae27107a3cf356872
Birky-andrie <145018370+Birky-andrie@users.noreply.github.com>
2023-09-14 05:13:17 -0700

    Add files via upload
    Worked together with Cielo Salas

---
+++ ACT1 Pacuribot Salas.py
@@ -0,0 +1,12 @@
+# This will print my information along with "Cielo"
+print("Birky Andrie Pacuribot")
+
+print("BSCS")
+
+print("CS1D")
+
+print("19")
+
+print("November 28, 2003")
+
+print("El Salvador, Poblacion Zone 4")
